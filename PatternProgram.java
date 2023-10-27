9c54b0f6f4c69485c209c4b79150a2f41d05c999
pawarakshay136 <140945443+pawarakshay136@users.noreply.github.com>
2023-10-27 15:26:01 +0530

    Create PatternProgram.java

---
+++ PatternProgram.java
@@ -0,0 +1,13 @@
+for (int i=1;i<=5;i++) {
+			for (int j=1;j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}  // This Loop is to generate upper Triangle
+
+		for (int i=1;i<=5;i++) {
+			for (int j=4;j>=i;j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		} 
